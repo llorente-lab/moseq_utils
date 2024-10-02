9f801bd7d59f4438bf9e7fd596fce5fb1e66ed9c
athp18 <129908089+athp18@users.noreply.github.com>
2024-10-01 19:24:59 -0700

    update to use mappings from config

--- visualization/usage_plots.py
+++ visualization/usage_plots.py
@@ -12,30 +12,7 @@ import os
 from scikit_posthocs import posthoc_dunn
 import matplotlib as mpl
 from scipy.spatial.distance import jensenshannon
-
-MAPPING = {
-    'WMS_Baseline': 'White Matter Stroke (Baseline, Female)',
-    'WMS_7dpi': 'White Matter Stroke (7 dpi, Female)',
-    'WMS_30dpi': 'White Matter Stroke (30 dpi, Female)',
-    'CS_Baseline': 'Cortical Stroke (Baseline, Female)',
-    'CS_7dpi': 'Cortical Stroke (7 dpi, Female)',
-    'CS_30dpi': 'Cortical Stroke (30 dpi, Female)',
-    'Control': 'Control (Baseline, Female)',
-    'Control_7dpi': 'Control (7 days, Female)',
-    'Control_30dpi': 'Control (30 days, Female)'
-}
-
-COLOR_MAPPING = {
-    'White Matter Stroke (Baseline, Female)': '#1f77b4',
-    'White Matter Stroke (7 dpi, Female)': '#ff7f0e',
-    'White Matter Stroke (30 dpi, Female)': '#4c72b0',
-    'Cortical Stroke (Baseline, Female)': '#dd8452',
-    'Cortical Stroke (7 dpi, Female)': '#a1c9f4',
-    'Cortical Stroke (30 dpi, Female)': '#ffb482',
-    'Control (Baseline, Female)': '#4878d0',
-    'Control (7 days, Female)': '#ee854a',
-    'Control (30 days, Female)': '#001c7f'
-}
+from config import MAPPING, COLOR_MAPPING
 
 def run_kw_dunn(data, group_col, value_col, control_group):
     """
