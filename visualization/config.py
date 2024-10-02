1ee4d279188c8a1c58d6d97499f2e3f5bc3dc875
athp18 <129908089+athp18@users.noreply.github.com>
2024-10-01 21:56:01 -0700

    separate into male and female

--- visualization/config.py
+++ visualization/config.py
@@ -2,7 +2,7 @@
 Utility configs
 """
 
-MAPPING = {
+MAPPING_FEMALES = {
     'WMS_Baseline': 'White Matter Stroke (Baseline, Female)',
     'WMS_7dpi': 'White Matter Stroke (7 dpi, Female)',
     'WMS_30dpi': 'White Matter Stroke (30 dpi, Female)',
@@ -14,7 +14,7 @@ MAPPING = {
     'Control_30dpi': 'Control (30 days, Female)'
 }
 
-COLOR_MAPPING = {
+COLOR_MAPPING_FEMALES = {
     'White Matter Stroke (Baseline, Female)': '#1f77b4',
     'White Matter Stroke (7 dpi, Female)': '#ff7f0e',
     'White Matter Stroke (30 dpi, Female)': '#4c72b0',
@@ -24,4 +24,28 @@ COLOR_MAPPING = {
     'Control (Baseline, Female)': '#4878d0',
     'Control (7 days, Female)': '#ee854a',
     'Control (30 days, Female)': '#001c7f'
+}
+
+MAPPING_MALES = {
+    'WMS_Baseline': 'White Matter Stroke (Baseline, Male)',
+    'WMS_7dpi': 'White Matter Stroke (7 dpi, Male)',
+    'WMS_30dpi': 'White Matter Stroke (30 dpi, Male)',
+    'CS_Baseline': 'Cortical Stroke (Baseline, Male)',
+    'CS_7dpi': 'Cortical Stroke (7 dpi, Male)',
+    'CS_30dpi': 'Cortical Stroke (30 dpi, Male)',
+    'Control_bsl': 'Control (Baseline, Male)',
+    'Control_7dpi': 'Control (7 days, Male)',
+    'Control_30dpi': 'Control (30 days, Male)'
+}
+
+COLOR_MAPPING_MALES = {
+    'White Matter Stroke (Baseline, Male)': '#1f77b4',
+    'White Matter Stroke (7 dpi, Male)': '#ff7f0e',
+    'White Matter Stroke (30 dpi, Male)': '#4c72b0',
+    'Cortical Stroke (Baseline, Male)': '#dd8452',
+    'Cortical Stroke (7 dpi, Male)': '#a1c9f4',
+    'Cortical Stroke (30 dpi, Male)': '#ffb482',
+    'Control (Baseline, Male)': '#e377c2',            
+    'Control (7 days, Male)': '#7f7f7f',               
+    'Control (30 days, Male)': '#bcbd22' 
 }
