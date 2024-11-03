fd996f4dc980ebbcafd89a31e7094b2a7d8d4b4d
athp18 <129908089+athp18@users.noreply.github.com>
2024-11-03 11:22:09 -0800

    Update classifier_tensorflow.py

--- classifier/classifier_tensorflow.py
+++ classifier/classifier_tensorflow.py
@@ -21,8 +21,71 @@ from sklearn.metrics import (
 from sklearn.utils import class_weight
 from imblearn.over_sampling import SMOTE
 import tensorflow_decision_forests as tfdf
-from plotting.config import get_mapping
+#from plotting.config import get_mapping
 
+"""
+Utility configs
+"""
+
+MAPPING_FEMALES = {
+    "WMS_Baseline": "White Matter Stroke (Baseline, Female)",
+    "WMS_7dpi": "White Matter Stroke (7 dpi, Female)",
+    "WMS_30dpi": "White Matter Stroke (30 dpi, Female)",
+    "CS_Baseline": "Cortical Stroke (Baseline, Female)",
+    "CS_7dpi": "Cortical Stroke (7 dpi, Female)",
+    "CS_30dpi": "Cortical Stroke (30 dpi, Female)",
+    "Control": "Control (Baseline, Female)",
+    "Control_7dpi": "Control (7 days, Female)",
+    "Control_30dpi": "Control (30 days, Female)",
+}
+
+COLOR_MAPPING_FEMALES = {
+    "White Matter Stroke (Baseline, Female)": "#1f77b4",
+    "White Matter Stroke (7 dpi, Female)": "#ff7f0e",
+    "White Matter Stroke (30 dpi, Female)": "#4c72b0",
+    "Cortical Stroke (Baseline, Female)": "#dd8452",
+    "Cortical Stroke (7 dpi, Female)": "#a1c9f4",
+    "Cortical Stroke (30 dpi, Female)": "#ffb482",
+    "Control (Baseline, Female)": "#4878d0",
+    "Control (7 days, Female)": "#ee854a",
+    "Control (30 days, Female)": "#001c7f",
+}
+
+MAPPING_MALES = {
+    "WMS_Baseline": "White Matter Stroke (Baseline, Male)",
+    "WMS_7dpi": "White Matter Stroke (7 dpi, Male)",
+    "WMS_30dpi": "White Matter Stroke (30 dpi, Male)",
+    "CS_Baseline": "Cortical Stroke (Baseline, Male)",
+    "CS_7dpi": "Cortical Stroke (7 dpi, Male)",
+    "CS_30dpi": "Cortical Stroke (30 dpi, Male)",
+    "Control_bsl": "Control (Baseline, Male)",
+    "Control_7dpi": "Control (7 days, Male)",
+    "Control_30dpi": "Control (30 days, Male)",
+}
+
+COLOR_MAPPING_MALES = {
+    "White Matter Stroke (Baseline, Male)": "#1f77b4",
+    "White Matter Stroke (7 dpi, Male)": "#ff7f0e",
+    "White Matter Stroke (30 dpi, Male)": "#4c72b0",
+    "Cortical Stroke (Baseline, Male)": "#dd8452",
+    "Cortical Stroke (7 dpi, Male)": "#a1c9f4",
+    "Cortical Stroke (30 dpi, Male)": "#ffb482",
+    "Control (Baseline, Male)": "#e377c2",
+    "Control (7 days, Male)": "#7f7f7f",
+    "Control (30 days, Male)": "#bcbd22",
+}
+
+
+def get_mapping(mapping):
+    """
+    Returns mappings as a tuple of (mapping, color_mapping)
+    """
+    if mapping.lower() == "male":
+        return MAPPING_MALES, COLOR_MAPPING_MALES
+    elif mapping.lower() == "female":
+        return MAPPING_FEMALES, COLOR_MAPPING_FEMALES
+    else:
+        raise ValueError(f"Mapping must be male or female, not {mapping}")
 
 class Classifier:
     """
@@ -86,6 +149,7 @@ class Classifier:
         self.num_classes = num_classes
         self.input_dim = input_dim
         self.label_encoder = label_encoder
+        self.num_epochs = num_epochs
 
         if not os.path.exists(self.plot_dir):
             os.makedirs(self.plot_dir)
@@ -94,7 +158,7 @@ class Classifier:
             n_splits=10, shuffle=True, random_state=self.random_state
         )
         
-        self.color_mapping = color_mapping if color_mapping else {}
+        self.color_mapping = get_mapping(color_mapping) if color_mapping else {}
 
         self.scaler = StandardScaler()
         self.smote = SMOTE(random_state=self.random_state)
